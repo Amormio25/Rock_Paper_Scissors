25e7d3f210647bdd7fecc66c53fe7dfaf10eb545
Amormio Velasquez III <amorbuks25@gmail.com>
2024-07-25 12:14:11 -0700

    Modified user and cpu functions and created function for one round

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,31 +1,63 @@
 // test for external link console.log('hello')
 
+// score values
+let humanScore = 0,
+    computerScore = 0;
+
 // random computer choice code
 let getComputerChoice = () => {
     let rand = Math.random();
     let cpu;
-    if (rand <= 0.333) {
+    if (rand <= 1/3) {
         cpu = 'rock'
-    } else if (rand > 0.333 && rand <= 0.666) {
+    } else if (rand <= 2/3) {
         cpu = 'paper'
-    } else if (rand > 0.666) {
+    } else {
         cpu = 'scissors'
     }
-    console.log(cpu) 
+    console.log(`The computer chose ${cpu}!`);
+    return cpu;
 }
-getComputerChoice()
 
 // human choice
 let getHumanChoice = () => {
     let myChoice = prompt('Choose rock, paper, or scissors.');
-    if (myChoice === undefined || myChoice === null || myChoice === '') {
-        return getHumanChoice()
-    } else if (myChoice.toLowerCase() === 'rock') {
-        return myChoice = 'rock'
-    } else if (myChoice.toLowerCase() === 'paper') {
-        return myChoice = 'paper'
-    } else if (myChoice.toLowerCase() === 'scissors') {
-        return myChoice = 'scissors'
+    myChoice = myChoice.toLowerCase();
+    if (myChoice !== 'rock' && myChoice !== 'paper' && myChoice !== 'scissors') {
+        alert('Invalid option. Please try again.');
+        return getHumanChoice();    
+    } else {
+        console.log(`You chose ${myChoice}!`);
+        return myChoice;
     }
+ }
+
+ // one round
+let playRound = (humanChoice, computerChoice) => {
+    if (humanChoice === computerChoice) {
+        alert(`It\'s a tie! Both chose ${humanChoice}!`)
+    } else if (humanChoice === 'rock'  && computerChoice === 'paper') {
+        alert('You lose! Paper beats rock!')
+        computerScore++
+    } else if (humanChoice === 'paper'  && computerChoice === 'scissors') {
+        alert('You lose! Scissors beats paper!')
+        computerScore++
+    } else if (humanChoice === 'scissors'  && computerChoice === 'rock') {
+        alert('You lose! Rock beats scissors!')
+        computerScore++
+    } else if (humanChoice === 'rock'  && computerChoice === 'scissors') {
+        alert('You win! Rock beats scissors!')
+        humanScore++
+    } else if (humanChoice === 'paper'  && computerChoice === 'rock') {
+        alert('You win! Paper beats rock!')
+        humanScore++
+    } else if (humanChoice === 'scissors'  && computerChoice === 'paper') {
+        alert('You win! Scissors beats paper!')
+        humanScore++
+    }
+    alert(`Your score: ${humanScore}. Computer score: ${computerScore}.`)
 }
-getHumanChoice()
+
+let humanSelection = getHumanChoice()
+let computerSelection = getComputerChoice()
+playRound(humanSelection, computerSelection)
